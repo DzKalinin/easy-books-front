02c828b851d633ae3a19b5e3beff38ca68ec9166
Maksim Nizavets <mnizovec@gmail.com>
2020-07-01 22:52:52 +0300

    Deployed environment. Created basic routing. Build Header component (35%)

--- src/App.js
+++ src/App.js
@@ -1,11 +1,30 @@
-import React from 'react';
+import React from "react";
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link,
+  Redirect,
+} from "react-router-dom";
 
-function App() {
-  return (
-    <div className="App">
-     
-    </div>
-  );
+import { Header } from "./Header";
+import { Main } from "./Main";
+import { Footer } from "./Footer";
+
+class App extends React.Component {
+  state = {};
+
+  render() {
+    return (
+      <Router>
+        <Route path="/">
+          <Header />
+          <Main />
+          <Footer />
+        </Route>
+      </Router>
+    );
+  }
 }
 
 export default App;
